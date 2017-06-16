ca74e52d350dc1f0e4ed0a9b76fd748dc3f67dc6
frontend2 <frontend2gv8@gmail.com>
2017-06-16 15:11:15 -0300

    Adicionado mais coisas

--- source/js/_confis.js
+++ source/js/_confis.js
@@ -1,1 +1,27 @@
 const DOMINIO = window.location.protocol + '//' + window.location.host;
+
+var COMPONENTES = (function(){
+	function COMPONENTES(){
+		this._COMPONENTES = {};
+	}
+
+	COMPONENTES.prototype.define = function(elemento,Classe){
+		if(document.querySelector(elemento)){
+			this._COMPONENTES[elemento] = [];
+
+			var elementoList = document.querySelectorAll(elemento);
+
+			for(var i=0 ; i<elementoList.length ; i++){
+				this._COMPONENTES[elemento].push(new Classe(elementoList[i]));
+			}
+		}
+	}
+
+	COMPONENTES.prototype.log = function() {
+		return this;
+	};
+
+	return COMPONENTES;
+})();
+
+var elementosCustom = new COMPONENTES();
